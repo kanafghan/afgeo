170edca7d8c93a9bfde50345e0850fd713b87700
khannoori <khan_noori@msn.com>
2013-09-24 14:03:00 +0200

    implementering af ...

---
+++ gui/src/controllers/AboutController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import main.Game;
+
+public class AboutController extends GController {
+
+	public AboutController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ gui/src/controllers/MPWCController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import main.Game;
+
+public class MPWCController extends GController {
+
+	public MPWCController(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
