db80a7a94965b1fec745d9e10167b075a30365d8
khannoori <khan_noori@msn.com>
2013-09-26 11:51:42 +0200

    Tilføjelse til Gmodel

---
+++ gui/src/models/FPModel.java
@@ -0,0 +1,12 @@
+package models;
+
+import main.Game;
+
+public class FPModel extends GModel {
+
+	public FPModel(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ gui/src/models/MPWCModel.java
@@ -0,0 +1,12 @@
+package models;
+
+import main.Game;
+
+public class MPWCModel extends GModel {
+
+	public MPWCModel(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
